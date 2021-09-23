31e0043b481ee88fe704d9cc79596f7a4639ad73
James Strande <strandejr@gmail.com>
2021-09-23 08:35:09 -0400

    added comments describing steps

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ const useableNumbersLength = useableNumbers.length;
 const specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 const specialCharactersLength = specialCharacters.length;
 
-
+// keep track of the user's preferences 
 var userPreferences = {
   passwordLength: '',
   useLowerCase: '',
@@ -19,7 +19,9 @@ var userPreferences = {
   useNumbers: '',
   useSpecialCharacters: ''
 };
-var numUseableCharacters = 0;
+
+// prompt to get user's password length preference as a number with constraints min: 8, max: 128
+// all invalid responses will restart the getPassword function
 
 function getPasswordLengthPreference() {
   const length = Number(prompt('Please enter a number between 8-128: ', ''));
@@ -30,28 +32,28 @@ function getPasswordLengthPreference() {
     userPreferences.passwordLength = length;
   }
 }
-
+// lowercase characters confirmation prompt
 function getLowercaseResponse() {
   const lowercase = window.confirm('use lowercase?');
   userPreferences.useLowerCase = lowercase;
 }
-
+// uppercase characters confirmation prompt
 function getUppercaseResponse() {
   const uppercase = window.confirm('use uppercase?');
   userPreferences.useUpperCase = uppercase;
 }
-
+// numbers characters confirmation prompt
 function getNumbersResponse() {
   const numbers = window.confirm('use numbers?');
   userPreferences.useNumbers = numbers;
 }
-
+// special characters confirmation prompt
 function getSpecialCharsResponse() {
   const specialCharacters = window.confirm('use special characters?');
   userPreferences.useSpecialCharacters = specialCharacters;
 }
 
-
+// function calling the prompts for the user preferences 
 
 function getUserPreferences() {
   getPasswordLengthPreference();
@@ -62,6 +64,9 @@ function getUserPreferences() {
   // console.log(numUseableCharacters);
   // console.log(useableCharactersArray);
 }
+
+// function compiling the userPreferences and converting into a password string including and/or excluding preferences based on confirmation prompts
+// if no types of characters are confirmed (if user hits cancel on all confirmation prompts) this will create an invalid/undefined password
 
 function generatePassword() {
   var useableCharactersArray = [];
@@ -102,6 +107,8 @@ function generatePassword() {
   } else {
     console.log(useableCharactersArray.length);
   };
+  // final if statement to validate whether user has input the minimum requirements resulting in a useableCharacterArray.length > 0
+  // if no character types are confirmed, this if statement will force user to start from the getPasswordLengthPreference via generatePassword calling getUserPreferences
   if (useableCharactersArray.length == 0) {
     return generatePassword();
   } else {
@@ -112,6 +119,9 @@ function generatePassword() {
   };
   return password;
 };
+
+// function calling the generatePassword function to initiate prompts 
+// displays the randomized password 
 
 function writePassword() { 
   password = generatePassword();
